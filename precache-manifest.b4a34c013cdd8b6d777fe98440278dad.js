d02cf2655ffefe65d87c85c9a6331343a85dacaf
pranika98 <pranikakakkar@gmail.com>
2020-05-06 13:09:15 +0530

    Updates

---
+++ precache-manifest.b4a34c013cdd8b6d777fe98440278dad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0357eaf899371a826b63c0e4304a977",
+    "url": "/Robo-Tracker/index.html"
+  },
+  {
+    "revision": "243256e45732985e0327",
+    "url": "/Robo-Tracker/static/css/2.3af2dac8.chunk.css"
+  },
+  {
+    "revision": "8e2e817f9ce8c5187206",
+    "url": "/Robo-Tracker/static/css/main.0a4b7e61.chunk.css"
+  },
+  {
+    "revision": "243256e45732985e0327",
+    "url": "/Robo-Tracker/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Tracker/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e2e817f9ce8c5187206",
+    "url": "/Robo-Tracker/static/js/main.1ff83150.chunk.js"
+  },
+  {
+    "revision": "2899ddc7584dd69b1c45",
+    "url": "/Robo-Tracker/static/js/runtime-main.11eef322.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Tracker/static/media/SEGA.12a3db8b.woff"
+  }
+]);
